fec8d3db46133070fe3396478b863b678acf4041
Kangze Huang <kangzeroo@gmail.com>
2019-02-24 05:05:10 -0500

    setup for ml engine cloud training

--- mlengine/setup.py
+++ mlengine/setup.py
@@ -1,8 +1,14 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES = ['Keras==2.0.4',
-                     'h5py==2.7.0']
+REQUIRED_PACKAGES = ['Keras==2.2.4',
+                     'h5py==2.9.0',
+                     'tensorflow==1.10.0',
+                     'numpy==1.14.5',
+                     'cv2==4.0.0',
+                     'PIL==5.4.1',
+                     'google',
+                     'os']
 
 setup(
     name='messy_room_ai',
