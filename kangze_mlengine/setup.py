6f63b976b354d6284c6acdf16f92f61ce1edf3c9
TangrisJones <cleland.theo@gmail.com>
2019-04-08 22:11:21 -0400

    added updated requirements for tf2

--- kangze_mlengine/setup.py
+++ kangze_mlengine/setup.py
@@ -1,8 +1,9 @@
 from setuptools import find_packages, setup
 
-REQUIRED_PACKAGES = ['Keras==2.2.4',
+REQUIRED_PACKAGES = ['tensorflow-gpu==2.0.0a0',
+                     'CUDA==10.0',
+                     'Keras==2.2.4',
                      'h5py==2.9.0',
-                     'tensorflow-gpu==2.0.0a0',
                      'numpy==1.16.2',
                      'Pillow==2.2.1',
                      'opencv-python==4.0.0.21',
@@ -10,6 +11,16 @@ REQUIRED_PACKAGES = ['Keras==2.2.4',
                      'google-cloud-storage==1.14.0',
                      'google-cloud==0.34.0',
                      'google-api-python-client==1.7.8']
+# REQUIRED_PACKAGES = ['tensorflow-gpu==2.0.0a0',
+#                      'Keras==2.2.4',
+#                      'h5py',
+#                      'numpy',
+#                      'Pillow',
+#                      'opencv-python',
+#                      'google',
+#                      'google-cloud-storage',
+#                      'google-cloud',
+#                      'google-api-python-client']
 
 setup(
     name='messy_room_ai',
